52b9a62d1c49e9b75353a802d20826c7a6f5c82b
Jesse Krim <jesse.krim@gmail.com>
2020-03-23 14:58:14 -0400

    Error in gRunner reducer

--- store/reducers/gRunner.js
+++ store/reducers/gRunner.js
@@ -2,17 +2,15 @@ import { SET_GRUNNERS } from '../actions/orders';
 
 const initialState = {
   gRunners: []
-}
+};
 
-export default (state = initialState, action) {
+export default (state = initialState, action) => {
   switch (action.type) {
     case SET_GRUNNERS:
-    return {
-      gRunners: action.gRunners
-    };
+      return {
+        gRunners: action.gRunners
+      };
     default:
       return state;
   }
-  }
-
-
+};
